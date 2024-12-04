7efddf8c583c605965e6312a36230c5385d58ac7
TuyetPhuongTong <phuongtong.31221023099@st.ueh.edu.vn>
2024-12-04 15:00:35 +0700

    Update color-delete.php

--- admin/color-delete.php
+++ admin/color-delete.php
@@ -1,12 +1,12 @@
 <?php require_once('header.php'); ?>
 
 <?php
-// Preventing the direct access of this page.
+// Ngăn chặn truy cập trực tiếp vào trang này.
 if(!isset($_REQUEST['id'])) {
 	header('location: logout.php');
 	exit;
 } else {
-	// Check the id is valid or not
+	// Kiểm tra xem id có hợp lệ hay không
 	$statement = $pdo->prepare("SELECT * FROM tbl_color WHERE color_id=?");
 	$statement->execute(array($_REQUEST['id']));
 	$total = $statement->rowCount();
@@ -19,7 +19,7 @@ if(!isset($_REQUEST['id'])) {
 
 <?php
 
-	// Delete from tbl_color
+	// Xóa khỏi bảng tbl_color
 	$statement = $pdo->prepare("DELETE FROM tbl_color WHERE color_id=?");
 	$statement->execute(array($_REQUEST['id']));
 

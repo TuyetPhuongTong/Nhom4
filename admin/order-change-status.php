923980a479645bbbe2b66c411a4d54268a270e6d
TuyetPhuongTong <phuongtong.31221023099@st.ueh.edu.vn>
2024-12-05 12:32:53 +0700

    Update order-change-status.php

--- admin/order-change-status.php
+++ admin/order-change-status.php
@@ -5,7 +5,7 @@ if( !isset($_REQUEST['id']) || !isset($_REQUEST['task']) ) {
 	header('location: logout.php');
 	exit;
 } else {
-	// Check the id is valid or not
+	// Kiểm tra ID có hợp lệ hay không
 	$statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE id=?");
 	$statement->execute(array($_REQUEST['id']));
 	$total = $statement->rowCount();

3398e3d11e9b9be1a0925c2016c27b3a1d53842e
TuyetPhuongTong <phuongtong.31221023099@st.ueh.edu.vn>
2024-12-06 17:08:19 +0700

    Update order-delete.php

--- admin/order-delete.php
+++ admin/order-delete.php
@@ -2,17 +2,20 @@
 
 <?php
 if(!isset($_REQUEST['id'])) {
+	 // Nếu không có tham số 'id' trong yêu cầu, chuyển hướng đến trang đăng xuất
 	header('location: logout.php');
 	exit;
 } else {
-	// Check tài khoản có tồn tại không
+	// Kiểm tra xem id có hợp lệ hay không
 	$statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE id=?");
 	$statement->execute(array($_REQUEST['id']));
 	$total = $statement->rowCount();
 	if( $total == 0 ) {
+		// Nếu không tìm thấy, chuyển hướng đến trang đăng xuất
 		header('location: logout.php');
 		exit;
 	} else {
+		// Lấy dữ liệu liên quan đến thanh toán
 		$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
 		foreach ($result as $row) {
 			$payment_id = $row['payment_id'];
@@ -24,11 +27,11 @@ if(!isset($_REQUEST['id'])) {
 ?>
 
 <?php
-	
+// Nếu thanh toán và vận chuyển đã hoàn tất, không hoàn lại số lượng sản phẩm
 	if( ($payment_status == 'Completed') && ($shipping_status == 'Completed') ):
-		// No return to stock
+		// Không hoàn lại số lượng sản phẩm
 	else:
-		// Return the stock
+		// Hoàn lại số lượng sản phẩm vào kho
 		$statement = $pdo->prepare("SELECT * FROM tbl_order WHERE payment_id=?");
 		$statement->execute(array($payment_id));
 		$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
@@ -45,13 +48,13 @@ if(!isset($_REQUEST['id'])) {
 		}	
 	endif;	
 
-	// Delete from tbl_order
+	// Xóa dữ liệu liên quan khỏi bảng tbl_order
 	$statement = $pdo->prepare("DELETE FROM tbl_order WHERE payment_id=?");
 	$statement->execute(array($payment_id));
 
-	// Delete from tbl_payment
+	// Xóa dữ liệu thanh toán khỏi bảng tbl_payment
 	$statement = $pdo->prepare("DELETE FROM tbl_payment WHERE id=?");
 	$statement->execute(array($_REQUEST['id']));
-
+    // Chuyển hướng đến trang đơn hàng
 	header('location: order.php');
 ?>

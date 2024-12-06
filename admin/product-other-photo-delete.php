3cdfb1d928097ed5c986de4a475a0472e9e1a91c
TuyetPhuongTong <phuongtong.31221023099@st.ueh.edu.vn>
2024-12-06 22:16:02 +0700

    Update product-other-photo-delete.php

--- admin/product-other-photo-delete.php
+++ admin/product-other-photo-delete.php
@@ -1,16 +1,17 @@
 <?php require_once('header.php'); ?>
 
 <?php
+// Kiểm tra xem tham số `id` và `id1` có được gửi qua URL không
 if( !isset($_REQUEST['id']) || !isset($_REQUEST['id1']) ) {
 	header('location: logout.php');
 	exit;
 } else {
-	// Check the id is valid or not
+	// Kiểm tra tính hợp lệ của `id`
 	$statement = $pdo->prepare("SELECT * FROM tbl_product_photo WHERE pp_id=?");
 	$statement->execute(array($_REQUEST['id']));
 	$total = $statement->rowCount();
 	if( $total == 0 ) {
-		header('location: logout.php');
+		header('location: logout.php');// Nếu `id` không hợp lệ, chuyển hướng đến trang đăng xuất
 		exit;
 	}
 }
@@ -18,22 +19,22 @@ if( !isset($_REQUEST['id']) || !isset($_REQUEST['id1']) ) {
 
 <?php
 
-	// Getting photo ID to unlink from folder
+	// Lấy thông tin ảnh cần xóa khỏi thư mục
 	$statement = $pdo->prepare("SELECT * FROM tbl_product_photo WHERE pp_id=?");
 	$statement->execute(array($_REQUEST['id']));
 	$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
 	foreach ($result as $row) {
-		$photo = $row['photo'];
+		$photo = $row['photo'];// Lấy tên file ảnh
 	}
 
-	// Unlink the photo
+	// Xóa ảnh khỏi thư mục
 	if($photo!='') {
 		unlink('../assets/uploads/product_photos/'.$photo);	
 	}
 
-	// Delete from tbl_testimonial
+	// Xóa ảnh khỏi cơ sở dữ liệu tbl_testimonial
 	$statement = $pdo->prepare("DELETE FROM tbl_product_photo WHERE pp_id=?");
 	$statement->execute(array($_REQUEST['id']));
-
+    // Chuyển hướng quay lại trang chỉnh sửa sản phẩm
 	header('location: product-edit.php?id='.$_REQUEST['id1']);
 ?>

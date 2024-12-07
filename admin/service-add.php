0bb8aa5d402b8006260c3abe6521b38bb404718c
TuyetPhuongTong <phuongtong.31221023099@st.ueh.edu.vn>
2024-12-07 20:43:15 +0700

    Update service-add.php

--- admin/service-add.php
+++ admin/service-add.php
@@ -57,10 +57,10 @@ if(isset($_POST['form1'])) {
 
 <section class="content-header">
 	<div class="content-header-left">
-		<h1>Add Service</h1>
+		<h1>Thêm Dịch vụ</h1>
 	</div>
 	<div class="content-header-right">
-		<a href="service.php" class="btn btn-primary btn-sm">View All</a>
+		<a href="service.php" class="btn btn-primary btn-sm">Xem tất cả</a>
 	</div>
 </section>
 
@@ -108,7 +108,7 @@ if(isset($_POST['form1'])) {
 						<div class="form-group">
 							<label for="" class="col-sm-2 control-label"></label>
 							<div class="col-sm-6">
-								<button type="submit" class="btn btn-success pull-left" name="form1">Submit</button>
+								<button type="submit" class="btn btn-success pull-left" name="form1">Thêm</button>
 							</div>
 						</div>
 					</div>

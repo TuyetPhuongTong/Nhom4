55a0eda856cc993f969e033da8ee26fbc177dddd
Giang <giangchch719@gmail.com>
2024-12-10 14:47:55 +0700

    Update top-category-add.php

--- admin/top-category-add.php
+++ admin/top-category-add.php
@@ -6,7 +6,7 @@ if(isset($_POST['form1'])) {
 
     if(empty($_POST['tcat_name'])) {
         $valid = 0;
-        $error_message .= "Tên danh mục cấp cao không được để trống<br>";
+        $error_message .= "Tên danh mục sản phẩm không được để trống<br>";
     } else {
     	// Kiểm tra danh mục trùng lặp
     	$statement = $pdo->prepare("SELECT * FROM tbl_top_category WHERE tcat_name=?");
@@ -15,7 +15,7 @@ if(isset($_POST['form1'])) {
     	if($total)
     	{
     		$valid = 0;
-        	$error_message .= "Tên danh mục cấp cao đã tồn tại<br>";
+        	$error_message .= "Tên danh mục sản phẩm đã tồn tại<br>";
     	}
     }
 
@@ -25,14 +25,14 @@ if(isset($_POST['form1'])) {
 		$statement = $pdo->prepare("INSERT INTO tbl_top_category (tcat_name,show_on_menu) VALUES (?,?)");
 		$statement->execute(array($_POST['tcat_name'],$_POST['show_on_menu']));
 	
-    	$success_message = 'Danh mục cấp cao đã được thêm thành công.';
+    	$success_message = 'Danh mục sản phẩm đã được thêm thành công.';
     }
 }
 ?>
 
 <section class="content-header">
 	<div class="content-header-left">
-		<h1>Thêm danh mục cấp cao</h1>
+		<h1>Thêm danh mục sản phẩm</h1>
 	</div>
 	<div class="content-header-right">
 		<a href="top-category.php" class="btn btn-primary btn-sm">Xem tất cả</a>
@@ -61,7 +61,7 @@ if(isset($_POST['form1'])) {
 				<div class="box box-info">
 					<div class="box-body">
 						<div class="form-group">
-							<label for="" class="col-sm-2 control-label">Tên danh mục cấp cao <span>*</span></label>
+							<label for="" class="col-sm-2 control-label">Tên danh mục sản phẩm <span>*</span></label>
 							<div class="col-sm-4">
 								<input type="text" class="form-control" name="tcat_name">
 							</div>
@@ -78,7 +78,7 @@ if(isset($_POST['form1'])) {
 						<div class="form-group">
 							<label for="" class="col-sm-2 control-label"></label>
 							<div class="col-sm-6">
-								<button type="submit" class="btn btn-success pull-left" name="form1">Gửi</button>
+								<button type="submit" class="btn btn-success pull-left" name="form1">Thêm</button>
 							</div>
 						</div>
 					</div>

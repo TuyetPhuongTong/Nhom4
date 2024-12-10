7200c78787102d20691e5ef6cb9c5c6ff4d14490
Giang <giangchch719@gmail.com>
2024-12-10 16:36:45 +0700

    Update header.php

--- header.php
+++ header.php
@@ -402,7 +402,7 @@ foreach ($result as $row) {
 				<div class="menu-container" style="text-align: center;">
 					<div class="menu">
 						<ul style="list-style-type: none; padding: 0; margin: 0; background-color: #7f572e;">
-							<li style="display: inline-block; margin-right: 10px; background-color: #7f572e;">
+							<li style="display: inline-block; margin-right: 20px; background-color: #7f572e;">
 								<a href="index.php" style="color: #f6dbab; font-size: 16px; text-decoration: none; padding: 10px 15px; display: inline-block;">Home</a>
 							</li>
 							
@@ -490,5 +490,4 @@ foreach ($result as $row) {
 		</div>
 	</div>
 </div>
-
 

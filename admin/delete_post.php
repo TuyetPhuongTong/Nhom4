617fe6c62ea5d986613dcc8f6eb761db2d12585b
Như Ý <trannhuy270304@gmail.com>
2024-12-10 15:37:39 +0700

    Delete delete_post.php

--- admin/delete_post.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('header.php');
-
-if (isset($_GET['id'])) {
-    $statement = $pdo->prepare("DELETE FROM tbl_posts WHERE id=?");
-    $statement->execute([$_GET['id']]);
-    header("Location: manage_posts.php");
-}
-?>

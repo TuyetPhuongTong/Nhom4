4268e2002fb5e529908ac87e6ee9355470cd37a0
haunguyen <haunguyen.31221024402@st.ueh.edu.vn>
2024-12-10 12:53:29 +0700

    Update header.php

--- header.php
+++ header.php
@@ -402,6 +402,9 @@ foreach ($result as $row) {
 							<li><a href="faq.php"><?php echo $faq_title; ?></a></li>
 
 							<li><a href="contact.php"><?php echo $contact_title; ?></a></li>
+							<li class="nav-item">
+                            <a class="nav-link" href="blog.php">Blog</a></li>
+
 						</ul>
 					</div>
 				</div>

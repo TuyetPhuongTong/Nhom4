658230cf9719d59519f8e658613ec0b2d432eef1
Như Ý <trannhuy270304@gmail.com>
2024-12-10 13:02:51 +0700

    Update post.php

--- admin/post.php
+++ admin/post.php
@@ -51,11 +51,11 @@ if (isset($_POST['form_add_post'])) {
 
         // Cập nhật cơ sở dữ liệu
          $statement = $pdo->prepare("UPDATE tbl_post SET post_id=?, post_title=?,post_slug=?,post_content=?,post_date=?,photo=?,category_id=?,total_view=?,meta_title=? WHERE post_id=1");
-         $statement->execute(array($_POST['post_title'],$_POST['post_slug'],$_POST['post_content'],$_POST['photo'],$final_name ?,$_POST['post_date'],$_POST['category_id'],$_POST['total_view'],$_POST['meta_title']));
+         $statement->execute(array($_POST['post_id'],$_POST['post_title'],$_POST['post_slug'],$_POST['post_content'],$final_name?:$photo,$_POST['post_date'],$_POST['category_id'],$_POST['total_view'],$_POST['meta_title']));
      } else {
 //CẬP NHẬT DỮ LIỆU
-          $statement = $pdo->prepare("UPDATE tbl_post SET post_title=?,post_slug=?,post_content=?,post_date=?,photo=?,category_id=?,total_view=?,meta_title=? WHERE post_id=1");
-          $statement->execute(array($_POST['post_title'],$_POST['post_slug'],$_POST['post_content'],$_POST['post_date'],$_POST['photo'],$_POST['category_id'],$_POST['total_view'],$_POST['meta_title']));
+          $statement = $pdo->prepare("UPDATE tbl_post SET post_id=?,post_title=?,post_slug=?,post_content=?,post_date=?,photo=?,category_id=?,total_view=?,meta_title=? WHERE post_id=1");
+          $statement->execute(array($_POST['post_id'],$_POST['post_title'],$_POST['post_slug'],$_POST['post_content'],$_POST['post_date'],$_POST['photo'],$_POST['category_id'],$_POST['total_view'],$_POST['meta_title']));
      }
      if ($statement->rowCount() > 0) {
         $success_message = 'Thông tin bài viết đã được cập nhật thành công.';

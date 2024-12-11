5a934b758bb9449cc74e7e2e6e78a156ec64520b
Như Ý <trannhuy270304@gmail.com>
2024-12-11 13:53:27 +0700

    Update header.php

--- header.php
+++ header.php
@@ -492,7 +492,7 @@ foreach ($result as $row) {
 									<?php echo $about_title; ?>
 								</a>
 							</li>
-							<li style="display: inline-block; margin-right: 20px; background-color: #7f572e; border-radius: 8px; overflow: hidden; width: 250px;">
+							<li style="display: inline-block; margin-right: 20px; background-color: #7f572e; ">
 								<a href="blog.php" style="color: #f6dbab; font-size: 16px; text-decoration: none; padding: 10px 15px; display: inline-block;">
 									<!-- Phần tiêu đề blog -->
 									<div style="padding: 10px; text-align: center;">

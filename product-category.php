e9ec589f4a9fac8d4563edfc8b35017483030d9d
Như Ý <trannhuy270304@gmail.com>
2024-12-13 19:42:41 +0700

    Update product-category.php

--- product-category.php
+++ product-category.php
@@ -235,7 +235,7 @@ if( !isset($_REQUEST['id']) || !isset($_REQUEST['type']) ) {
 .product-cat .row {
     display: flex;
     flex-wrap: wrap; /* Các phần tử sẽ xuống dòng khi hết không gian */
-    justify-content: space-between; /* Khoảng cách giữa các khối sản phẩm được chia đều */
+    justify-content: flex-start;; /* Khoảng cách giữa các khối sản phẩm được chia đều */
     gap: 20px; /* Khoảng cách giữa các khối */
 }
 

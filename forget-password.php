cedb1a790ba09d087abc7eca0ed8d137b9b71623
Giang <giangchch719@gmail.com>
2024-12-13 15:49:37 +0700

    Update forget-password.php

--- forget-password.php
+++ forget-password.php
@@ -64,7 +64,7 @@ if(isset($_POST['form1'])) {
 }
 ?>
 
-<div class="page-banner" style="background-color:#444;background-image: url(assets/uploads/<?php echo $banner_forget_password; ?>);">
+<div class="page-banner" style="background-color:#444;background-image: url(https://i.pinimg.com/736x/82/cd/4a/82cd4a8c262b9c774f0638d7fd88997a.jpg);">
     <div class="inner">
         <h1><?php echo "Quên mật khẩu"; ?></h1>
     </div>

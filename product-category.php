c3f1196ba5254a2018ea6de34f156c9fa69890ad
Như Ý <trannhuy270304@gmail.com>
2024-12-14 11:57:09 +0700

    Update product-category.php

--- product-category.php
+++ product-category.php
@@ -168,15 +168,15 @@ if( !isset($_REQUEST['id']) || !isset($_REQUEST['type']) ) {
 
 /* Từng khối sản phẩm */
 .product-cat .item-product-cat {
-    flex: 0 0 calc(33.333% - 20px); /* Chia mỗi khối sản phẩm thành 1/3 hàng */
-    box-sizing: border-box; /* Bao gồm padding và viền trong kích thước */
+    flex: 0 0 calc(33.333% - 10px); /* Chia mỗi khối sản phẩm thành 1/3 hàng */
+    min-width: 300px; /* Đảm bảo chiều rộng tối thiểu */
     display: flex;
     flex-direction: column; /* Xếp các phần tử con theo chiều dọc */
-    justify-content: space-between; /* Dàn đều các phần tử */
-    height: 100%; /* Đồng nhất chiều cao */
+    justify-content:  flex-start; /* Dàn đều các phần tử */
+    min-height: 450px;
     border: 1px solid #e1e1e1; /* Viền màu xám nhạt */
     border-radius: 10px; /* Bo góc khối */
-    margin-bottom: 20px; /* Khoảng cách dưới */
+   
     overflow: hidden; /* Ẩn nội dung tràn */
     transition: all 0.3s ease; /* Hiệu ứng chuyển đổi mượt */
 }

299875abaaaa77fbd7245abfa5c3f79199b5b3de
TuyetPhuongTong <phuongtong.31221023099@st.ueh.edu.vn>
2024-12-14 18:00:10 +0700

    Update social-media.php

--- admin/social-media.php
+++ admin/social-media.php
@@ -239,7 +239,7 @@ foreach ($result as $row) {
 						<div class="form-group">
 							<label for="" class="col-sm-2 control-label"></label>
 							<div class="col-sm-6">
-								<button type="submit" class="btn btn-success pull-left" name="form1">Submit</button>
+								<button type="submit" class="btn btn-success pull-left" name="form1">Cập nhật</button>
 							</div>
 						</div>
 					</div>

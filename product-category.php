1abad055bcbd9fdc23bcc6a48d88ec0658757e13
Như Ý <trannhuy270304@gmail.com>
2024-12-14 11:14:39 +0700

    Update product-category.php

--- product-category.php
+++ product-category.php
@@ -153,7 +153,7 @@ if( !isset($_REQUEST['id']) || !isset($_REQUEST['type']) ) {
 /* PHẦN 3: Hiển thị danh sách sản phẩm */
 .product-cat .row {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr)); /* Tăng kích thước tối thiểu của khung sản phẩm */
+    grid-template-columns: repeat(auto-fill, minmax(1fr, 350px)); /* Tăng kích thước tối thiểu của khung sản phẩm */
     gap: 30px; /* Tăng khoảng cách giữa các khối */
 }
 
